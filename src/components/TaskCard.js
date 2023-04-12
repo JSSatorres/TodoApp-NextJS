90255761cd0c5823e29cbd25d409cdeedb0d553e
jssatorres <juansataz.dev@gmail.com>
2023-04-12 18:25:13 +0200

    Feat (TaskCard) adding button to taskard

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -15,7 +15,19 @@ export const TaskCard = ({ task }) => {
       <div>
         <div className="flex justify-between">
           <h1 className="font-bold">{task.title}</h1>
-
+          <button
+            className="bg-red-700 hover:bg-red-600 px-3 py-1 inline-flex items-center"
+            onClick={(e) => {
+              e.stopPropagation();
+              const accept = confirm(
+                "Are you sure you want to delete this task?"
+              );
+              if (accept) deleteTask(task.id);
+              toast.success("Task deleted successfully");
+            }}
+          >
+            <VscTrash className="mr-2" /> Delete
+          </button>
         </div>
         <p className="text-gray-300">{task.description}</p>
         <span className="text-gray-400 text-xs">{task.id}</span>
